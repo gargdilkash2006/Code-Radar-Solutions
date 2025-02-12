6a15b96007c01000d62e85ff2b6eacd63edc1f53
Dilkash Garg <gargdilkash@gmail.com>
2025-02-12 16:39:00 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -3,19 +3,19 @@ int main() {
     char c;
     scanf("%c",c);
     if(c=='A') {
-        printf("Excellent");
+        printf("Excellent",%c);
     }
     else if(c=='B') {
-        printf("Good");
+        printf("Good",%c);
     }
     else if(c=='C') {
-        printf("Average");
+        printf("Average",%c);
     }
     else if(c=='D') {
-        printf("Below Average");
+        printf("Below Average",%c);
     }
     else {
-        printf("Fail");
+        printf("Fail",%c);
     }
     return 0;
 }
