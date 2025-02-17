a0f17b46c5421e533ea6c2be3f718fc22a37c01b
DILKASH GARG <gargdilkash@gmail.com>
2025-02-18 02:47:08 +0530

    Solution to Check Even or Odd

--- Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
+++ Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main () {
-    int leap;
-    scanf("%d",&leap);
-    if(leap%400==0 || (leap%4==0 && leap%100!=0)){
-        printf("leap year");
+    int s;
+    scanf("%d",&s);
+    if(s%2==0) {
+        printf("Even");
     }
-    else {
-        printf("not a leap year");
+    else{
+        printf("Odd");
     }
-    }
+}
